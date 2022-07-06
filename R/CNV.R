f2f747b5dae52fa5b14b53826a58387c6c624f60
Michael Hall <michael.safe.browse@gmail.com>
2022-07-06 10:12:36 +0200

    commit

--- R/CNV.R
+++ R/CNV.R
@@ -11,10 +11,12 @@ CNV <- function(file = NA, Chromosome = NA){
   #x <- length(table$con.2.NA)
   #x <- seq(1,x,1)
   c <- ggplot(data = table, mapping = aes(x=Strt, y = con.2.NA))
-  c <- c + geom_point() + facet_grid(~Chrom)
+  
+  d <- c + geom_point() + facet_grid(~Chrom) + ggplot2::ggtitle("Copy Number Variation Control Sample") + ylab("CNV Ratio") + xlab("Position")
+  c <- c + geom_point() + geom_point(mapping = aes(x= Strt, y = D2.2.NA), color = "red",size=0.50) + facet_grid(~Chrom) 
+  c <- c + ggplot2::ggtitle("Copy Number Variation Mutant and Control Sample") + ylab("CNV Ratio") + xlab("Position")
+  print(d)
   print(c)
   return(table)
 }
-
-
 
